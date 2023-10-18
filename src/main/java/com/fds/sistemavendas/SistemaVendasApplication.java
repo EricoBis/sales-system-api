9b261ef7d201e53892949079ee366f2b4653b172
Érico Bis <ericobis.santos@gmail.com>
2023-10-18 18:02:15 -0300

    add | project base

---
+++ src/main/java/com/fds/sistemavendas/SistemaVendasApplication.java
@@ -0,0 +1,13 @@
+package com.fds.sistemavendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaVendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fds/sistemavendas/SistemaVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fds.sistemavendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

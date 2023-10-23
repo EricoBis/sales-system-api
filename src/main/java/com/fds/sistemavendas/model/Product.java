589c2ddc18ed19fea537638f0b616830d3db799f
Érico Bis <ericobis.santos@gmail.com>
2023-10-23 13:22:57 -0300

    update | Product model

--- src/main/java/com/fds/sistemavendas/model/Product.java
+++ src/main/java/com/fds/sistemavendas/model/Product.java
@@ -1,5 +1,46 @@
 package com.fds.sistemavendas.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Product {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String description;
+    private double price;
+
+    public Product(Long id, String description, double price) {
+        this.id = id;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
 }

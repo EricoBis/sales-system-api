30137e4ad824a2a64b19831b576902e32d5e78c1
Deborah <dcitrinf@gmail.com>
2023-10-23 18:05:06 -0300

    models de Budget e ItemRequest

--- src/main/java/com/fds/sistemavendas/model/Budget.java
+++ src/main/java/com/fds/sistemavendas/model/Budget.java
@@ -1,5 +1,43 @@
 package com.fds.sistemavendas.model;
 
+import java.util.List;
+
+import jakarta.persistence.Id;
+
 public class Budget {
+    @Id
+    private Long id;
+    private Long requestId;
+    private double requestCost;
+    private double taxCost;
+    private double discount;
+    private double totalCost;
+    private boolean done;
+    private List<ItemRequest> items;
+
+    public Budget(Long id, Long requestId, double requestCost, double taxCost, double discount, double totalCost,
+            List<ItemRequest> items) {
+        this.id = id;
+        this.requestId = requestId;
+        this.requestCost = requestCost;
+        this.taxCost = taxCost;
+        this.discount = discount;
+        this.totalCost = totalCost;
+        this.done = false;
+        this.items = items;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
     
 }
--- src/main/java/com/fds/sistemavendas/model/ItemRequest.java
+++ src/main/java/com/fds/sistemavendas/model/ItemRequest.java
@@ -1,5 +1,32 @@
 package com.fds.sistemavendas.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class ItemRequest {
+    @Id
+    private Long productId;
+    private int amount;
     
+    public ItemRequest(Long productId, int amount) {
+        this.productId = productId;
+        this.amount = amount;
+    }
+    
+    public Long getProductId() {
+        return productId;
+    }
+    
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+    
+    public int getAmount() {
+        return amount;
+    }
+    
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }

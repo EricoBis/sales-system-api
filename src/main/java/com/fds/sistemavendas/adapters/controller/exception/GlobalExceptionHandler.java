4811768e59d82a17c33012e5d0161f38ba4c635a
Érico Bis <ericobis.santos@gmail.com>
2023-11-06 00:05:19 -0300

    add | handleNotFoundException

--- src/main/java/com/fds/sistemavendas/adapters/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/com/fds/sistemavendas/adapters/controller/exception/GlobalExceptionHandler.java
@@ -1,7 +1,17 @@
 package com.fds.sistemavendas.adapters.controller.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNotFoundException(NoSuchElementException notFoundException) {
+        return new ResponseEntity<>(notFoundException.getMessage(), HttpStatus.NOT_FOUND);
+    }
 }

003b6857353f6f92c302e17e92d6e90b2f8a26d6
Érico Bis <ericobis.santos@gmail.com>
2023-11-05 23:39:44 -0300

    add | RestControllerAdvice class

---
+++ src/main/java/com/fds/sistemavendas/adapters/controller/exception/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.fds.sistemavendas.adapters.controller.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}

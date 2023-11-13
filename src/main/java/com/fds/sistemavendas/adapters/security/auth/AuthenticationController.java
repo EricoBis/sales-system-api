a64bf2cac822370c308a92730637c89d677b526c
Érico Bis <ericobis.santos@gmail.com>
2023-11-13 18:27:58 -0300

    modified | AuthController

--- src/main/java/com/fds/sistemavendas/adapters/security/auth/AuthenticationController.java
+++ src/main/java/com/fds/sistemavendas/adapters/security/auth/AuthenticationController.java
@@ -2,6 +2,7 @@ package com.fds.sistemavendas.adapters.security.auth;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class AuthenticationController {
 
     private final AuthenticationService service;

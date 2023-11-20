95151e53d47c3928bf801d57b6304be439d0d440
Érico Bis <ericobis.santos@gmail.com>
2023-11-20 12:56:01 -0300

    add | ClientDTO

---
+++ src/main/java/com/fds/sistemavendas/application/dto/ClientDTO.java
@@ -0,0 +1,26 @@
+package com.fds.sistemavendas.application.dto;
+
+public class ClientDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+
+    public ClientDTO(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
